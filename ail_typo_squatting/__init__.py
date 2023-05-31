ea840e0d1ef1608ef3aa307e1389e9f92eb5a051
David Cruciani <david.cruciani@circl.lu>
2023-05-31 09:23:03 +0200

    fix: [import] delete import from init

--- ail_typo_squatting/__init__.py
+++ ail_typo_squatting/__init__.py
@@ -1,21 +1,16 @@
-from ail_typo_squatting.typo import runAll
-
 from ail_typo_squatting.typo import formatOutput
 from ail_typo_squatting.typo import dnsResolving
 
 from ail_typo_squatting.typo import omission
 from ail_typo_squatting.typo import repetition
 from ail_typo_squatting.typo import changeOrder
-from ail_typo_squatting.typo import transposition
 from ail_typo_squatting.typo import replacement
 from ail_typo_squatting.typo import doubleReplacement
-from ail_typo_squatting.typo import keyboardInsertion
 from ail_typo_squatting.typo import addition
 from ail_typo_squatting.typo import missingDot
 from ail_typo_squatting.typo import stripDash
 from ail_typo_squatting.typo import vowelSwap
 from ail_typo_squatting.typo import addDash
-from ail_typo_squatting.typo import bitsquatting
 from ail_typo_squatting.typo import homoglyph
 from ail_typo_squatting.typo import commonMisspelling
 from ail_typo_squatting.typo import homophones
